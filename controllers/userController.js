f77fc0919811fd1a1b0e586b53852f98db79b79b
김민성 <yssj2049@gmail.com>
2019-01-24 23:02:06 +0900

    Upload userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,20 @@
+import routes from "../routes";
 export const getJoin = (req,res) => {
     res.render("join",{pageTitle : "Join"});
 };
 export const postJoin = (req,res) => {
-    console.log(req.body);
-    res.render("join",{pageTitle : "Join"});
-    
+    const {
+        body : {name, email,password,password2}
+    } = req;
+    if(password !== password2){
+        res.status(400);
+        res.render("join",{pageTitle : "Join"});
+
+    }else {
+        // To Do : Register User
+        // To do : Log user in
+        res.redirect(routes.home);
+    }
 };
 
 
