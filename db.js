3420f3459296c1151df93492853edfaafa35fa45
김민성 <yssj2049@gmail.com>
2019-01-24 20:55:45 +0900

    Upload db.js

--- db.js
+++ db.js
@@ -1,8 +1,47 @@
-const videos = [
+export const videos = [
     {
         id: 324393,
-        title: 'Video awesome',
-        description: 'This is something I love',
+        title: "Video awesome",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "",
+        creator: {
+            id: 123123123,
+            name: "Kimminseong",
+            email: "yssj2049@gmail.com",
+
+        }
+    },
+    {
+        id: 32423233,
+        title: "Video super",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "",
+        creator: {
+            id: 123123123,
+            name: "Kimminseong",
+            email: "yssj2049@gmail.com",
+
+        }
+    },
+    {
+        id: 1231234393,
+        title: "Video hello",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "",
+        creator: {
+            id: 123123123,
+            name: "Kimminseong",
+            email: "yssj2049@gmail.com",
+
+        }
+    },
+    {
+        id: 12312123393,
+        title: "Video hi",
+        description: "This is something I love",
         views: 24,
         videoFile: "",
         creator: {
