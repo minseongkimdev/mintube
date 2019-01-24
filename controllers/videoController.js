7903042bcb1daeb9bf2184cec22fb32eba3fb741
김민성 <yssj2049@gmail.com>
2019-01-24 20:56:01 +0900

    Upload videoController.js

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,5 @@
-
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+import {videos} from "../db";
+export const home = (req, res) => res.render("home", { pageTitle: "Home", videos});
 
 export const search = (req, res) => {
     const { query: { term: searchingBy }
@@ -7,9 +7,9 @@ export const search = (req, res) => {
     res.render("search", { pageTitle: "Search", searchingBy });
 
 
-}
+};
 
-export const videos = (req, res) => res.send("videos");
+// export const videos = (req, res) => res.send("videos");
 export const upload = (req, res) => res.send("upload", { pageTitle: Upload });
 export const videoDetail = (req, res) => res.send("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) => res.send("editVideos", { pageTitle: "Edit Videos" });
