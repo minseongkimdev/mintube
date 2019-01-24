830039a3c3a5b690fef4fe3be6b16f19826489c8
김민성 <yssj2049@gmail.com>
2019-01-24 22:48:58 +0900

    Upload userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,13 @@
-export const join = (req,res) => res.render("join",{pageTitle : "Join"});
+export const getJoin = (req,res) => {
+    res.render("join",{pageTitle : "Join"});
+};
+export const postJoin = (req,res) => {
+    console.log(req.body);
+    res.render("join",{pageTitle : "Join"});
+    
+};
+
+
 export const login = (req,res) => res.render("login",{pageTitle : "Login"});
 export const logout = (req,res) => res.render("logout",{pageTitle : "Logout"});
 export const users = (req,res) => res.render("users",{pageTitle : "Users"});
