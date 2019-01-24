355b1e0cb065384fe0f31be23e7436226a8baf5f
김민성 <yssj2049@gmail.com>
2019-01-24 19:59:20 +0900

    Upload db.js

---
+++ db.js
@@ -0,0 +1,17 @@
+const videos = [
+    {
+        id: 324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "",
+        creator: {
+            id: 123123123,
+            name: "Kimminseong",
+            email: "yssj2049@gmail.com",
+
+        }
+    }
+
+
+];
