fa840846ba91a7a039e5ca786310a9f4172dc53c
김민성 <yssj2049@gmail.com>
2019-01-24 22:17:45 +0900

    Upload videoController.js

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,7 +4,7 @@ export const home = (req, res) => res.render("home", { pageTitle: "Home", videos
 export const search = (req, res) => {
     const { query: { term: searchingBy }
     } = req;
-    res.render("search", { pageTitle: "Search", searchingBy });
+    res.render("search", { pageTitle: "Search", searchingBy, videos});
 
 
 };
