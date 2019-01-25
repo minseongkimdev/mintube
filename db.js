10223595e26f44897d01d259f34da2c183b24deb
김민성 <yssj2049@gmail.com>
2019-01-25 21:22:06 +0900

    Upload db.js

--- db.js
+++ db.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
 
 
-mongoose.cennect("mongodb://localhost:27017/MinTube",
-{
-    useNewUrlParser : true, 
-    useFindAndModify : false
+mongoose.connect(
+    "mongodb://localhost:27017/MinTube", 
+    {
+    useNewUrlParser: true,
+    useFindAndModify: false
 }
 );
 
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = error => console.log('❌ Error on DB Connection:${error}');
+db.once("open",handleOpen);
+db.on("error",handleError);
