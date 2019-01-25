0087697a8b94e15a71f0feda360f5d07c072e1c3
김민성 <yssj2049@gmail.com>
2019-01-25 20:56:07 +0900

    Upload db.js

--- db.js
+++ db.js
@@ -1,56 +1,10 @@
-export const videos = [
-    {
-        id: 324393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 123123123,
-            name: "Kimminseong",
-            email: "yssj2049@gmail.com",
-
-        }
-    },
-    {
-        id: 32423233,
-        title: "Video super",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 123123123,
-            name: "Kimminseong",
-            email: "yssj2049@gmail.com",
-
-        }
-    },
-    {
-        id: 1231234393,
-        title: "Video hello",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 123123123,
-            name: "Kimminseong",
-            email: "yssj2049@gmail.com",
-
-        }
-    },
-    {
-        id: 12312123393,
-        title: "Video hi",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 123123123,
-            name: "Kimminseong",
-            email: "yssj2049@gmail.com",
-
-        }
-    }
+import mongoose from "mongoose";
 
 
-];
+mongoose.cennect("mongodb://localhost:27017/MinTube",
+{
+    useNewUrlParser : true, 
+    useFindAndModify : false
+}
+);
+
