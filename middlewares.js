5eba88a99dcd0e6fbcc8d55db01091e474584713
김민성 <yssj2049@gmail.com>
2019-01-25 17:40:37 +0900

    Upload middlewares.js

--- middlewares.js
+++ middlewares.js
@@ -2,5 +2,9 @@ import routes from "./routes";
 export const localMiddleware = (req, res, next) => {
     res.locals.siteName = "MinTube";
     res.locals.routes = routes;
+    res.locals.user = {
+        isAuthenticated : true,
+        id : 1
+    };
     next();
 };
