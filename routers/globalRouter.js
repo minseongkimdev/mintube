6115069753a8294f08045cef21aefdc52b786f95
김민성 <yssj2049@gmail.com>
2019-01-25 17:40:54 +0900

    Upload globalRouter.js

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -7,17 +7,23 @@ import {
 import {
     getJoin,
     postJoin,
-    login,
+    getLogin,
+    postLogin,
     logout
 } from "../controllers/userController";
 
 const globalRouter = express.Router();
+
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
+
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
