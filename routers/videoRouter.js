0d87efa55ecc6a0fb86be1a72e9d7510adc518a4
김민성 <yssj2049@gmail.com>
2019-01-27 21:20:44 +0900

    Upload videoRouter.js

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -9,12 +9,13 @@ import {
     postUpload
 } from "../controllers/videoController";
 
+import {uploadVideo} from "../middlewares";
 
 const videoRouter = express.Router();
 
 // Upload 
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload,uploadVideo, postUpload);
 
 // Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
