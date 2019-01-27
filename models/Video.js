6535706d934e7af954d2cfdc7552d264106f0577
김민성 <yssj2049@gmail.com>
2019-01-27 19:44:28 +0900

    Upload Video.js

---
+++ models/Video.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema({
+    fileUrl: {
+        type: String,
+        required: "File URL is required"
+    },
+    title: {
+        type: String,
+        required: "Title is required"
+    },
+    description: String,
+    views: {
+        type: Number,
+        default: 0
+
+
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    },
+    commnets : [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Comment"
+    }]
+
+
+});
+
+const model = mongoose.model("Video", videoSchema);
+
+export default model;
