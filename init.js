27d497647bc79d77ba1bf0a4c057dfba81e3c5d6
김민성 <yssj2049@gmail.com>
2019-01-27 19:44:10 +0900

    Upload init.js

--- init.js
+++ init.js
@@ -1,11 +1,15 @@
-import {videos} from "./db";
+import {
+    videos
+} from "./db";
 import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
+import "./models/Video";
+import "./models/Comment";
 
 
 const PORT = process.env.PORT || 4000;
 
-const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
+const handleListening = () => console.log(`✅ Listening on : http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
