ebc4991a46b92bbda04cd5430c10545ddfaf4b87
김민성 <yssj2049@gmail.com>
2019-01-27 19:44:04 +0900

    Upload db.js

--- db.js
+++ db.js
@@ -8,10 +8,16 @@ mongoose.connect(
     useFindAndModify: false
 }
 );
-
+mongoose.connect(
+    process.env.MONGO_URL,
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }
+);
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅ Connected to DB");
-const handleError = error => console.log('❌ Error on DB Connection:${error}');
+const handleError = error => console.log(`❌ Error on DB Connection : ${error}`);
 db.once("open",handleOpen);
 db.on("error",handleError);
