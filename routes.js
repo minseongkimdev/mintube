1fe6772ad3e5d9ed9e36ec0fbfa17a904de0c000
김민성 <yssj2049@gmail.com>
2019-01-28 20:16:30 +0900

    Upload routes.js

--- routes.js
+++ routes.js
@@ -54,7 +54,7 @@ const routes = {
     },
     deleteVideo : id => {
         if(id) {
-            return `/videos${id}/delete`;
+            return `/videos/${id}/delete`;
         }else {
             return DELETE_VIDEO;
         }
