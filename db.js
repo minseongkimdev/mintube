e73824e063debcf04f7aa3a42f6dae1e40f65af8
김민성 <yssj2049@gmail.com>
2019-01-29 18:21:07 +0900

    Upload db.js

--- db.js
+++ db.js
@@ -2,15 +2,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 mongoose.connect(
-    process.env.MONGO_URL,
-    {
-    useNewUrlParser: true,
-    useFindAndModify: false
-}
+    process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }
 );
 mongoose.connect(
-    process.env.MONGO_URL,
-    {
+    process.env.MONGO_URL, {
         useNewUrlParser: true,
         useFindAndModify: false
     }
@@ -19,5 +17,5 @@ const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = error => console.log(`❌ Error on DB Connection : ${error}`);
-db.once("open",handleOpen);
-db.on("error",handleError);
+db.once("open", handleOpen);
+db.on("error", handleError);
