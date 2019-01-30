4d88feade99e1b3e2034113d5fb723c16142417a
김민성 <yssj2049@gmail.com>
2019-01-30 18:16:49 +0900

    Upload init.js

--- init.js
+++ init.js
@@ -1,11 +1,13 @@
+import dotenv from "dotenv";
+
 import {
     videos
 } from "./db";
 import app from "./app";
-import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
 import "./models/Comment";
+import "./models/User";
 
 
 const PORT = process.env.PORT || 4000;
