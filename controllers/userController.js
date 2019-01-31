4466144ece488d1990863e9224004629a85ae6dc
김민성 <yssj2049@gmail.com>
2019-01-31 21:09:01 +0900

    Upload userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,10 @@
-import routes from "../routes";
-import User from "../models/User";
+import routes from "../routes"
+import User from "../models/User"
 export const getJoin = (req, res) => {
     res.render("join", {
         pageTitle: "Join"
-    });
-};
+    })
+}
 export const postJoin = async (req, res) => {
     const {
         body: {
@@ -13,52 +13,53 @@ export const postJoin = async (req, res) => {
             password,
             password2
         }
-    } = req;
+    } = req
     if (password !== password2) {
-        res.status(400);
+        res.status(400)
         res.render("join", {
             pageTitle: "Join"
-        });
-
+        })
     } else {
         try {
             const user = await User.create({
                 name,
                 email
-            });
-            await User.register(user, password);
-
+            })
+            await User.register(user, password)
+            next()
         } catch (error) {
-            console.log(error);
+            console.log(error)
         }
 
         // To Do : Register User
         // To do : Log user in
-        res.redirect(routes.home);
     }
-};
+}
 
-
-export const getLogin = (req, res) => res.render("login", {
-    pageTitle: "Log in"
-});
+export const getLogin = (req, res) =>
+    res.render("login", {
+        pageTitle: "Log in"
+    })
 export const postLogin = (req, res) => {
-    res.redirect(routes.home);
-};
+    res.redirect(routes.home)
+}
 export const logout = (req, res) => {
     // To Do : Process Log Out
-    res.redirect(routes.home);
-
-};
-export const users = (req, res) => res.render("users", {
-    pageTitle: "Users"
-});
-export const userDetail = (req, res) => res.render("userDetail", {
-    pageTitle: "User Detail"
-});
-export const editProfle = (req, res) => res.render("editProfile", {
-    pageTitle: "Edit Profile"
-});
-export const changePassword = (req, res) => res.render("changePassword", {
-    pageTitle: "Change Password"
-});
+    res.redirect(routes.home)
+}
+export const users = (req, res) =>
+    res.render("users", {
+        pageTitle: "Users"
+    })
+export const userDetail = (req, res) =>
+    res.render("userDetail", {
+        pageTitle: "User Detail"
+    })
+export const editProfle = (req, res) =>
+    res.render("editProfile", {
+        pageTitle: "Edit Profile"
+    })
+export const changePassword = (req, res) =>
+    res.render("changePassword", {
+        pageTitle: "Change Password"
+    })
