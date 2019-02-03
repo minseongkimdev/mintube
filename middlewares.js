1b805695bbd629a4ca677577544447526c4977b2
김민성 <yssj2049@gmail.com>
2019-02-03 20:16:56 +0900

    Add onlyPulbic method to control the logged in user

--- middlewares.js
+++ middlewares.js
@@ -13,4 +13,12 @@ export const localMiddleware = (req, res, next) => {
     next();
 };
 
+export const onlyPublic = (req, res, next) => {
+    if (req.user) {
+        res.redirect(routes.home);
+    } else {
+        next();
+    }
+}
+
 export const uploadVideo = multerVideo.single("videoFile");
