95d18191b9a9afc2c4d3cbd08147fbde88e4271e
김민성 <yssj2049@gmail.com>
2019-02-03 20:31:44 +0900

    Upload globalRouter.js

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -28,6 +28,6 @@ globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPublic, logout);
 
 export default globalRouter;
