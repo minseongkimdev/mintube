cb4039f3c56558a48985cb5a7c44735a57de6512
김민성 <yssj2049@gmail.com>
2019-02-06 18:34:12 +0900

    Update GithubStrategy

--- passport.js
+++ passport.js
@@ -1,7 +1,12 @@
 import passport from "passport";
 import User from "./models/User";
-
+import GithubStrategy from "passport-github";
 passport.use(User.createStrategy());
 
+passport.use(new GithubStrategy({
+    clientID: process.env.GH_ID,
+    clientSecret: process.env.GH_SECRET,
+    callbackURL: "http://localhost:4000/auth/github/callback"
+}))
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
