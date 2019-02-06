bd219cefc5e3dac4211bfcce2f0247b98304cfe0
김민성 <yssj2049@gmail.com>
2019-02-07 00:37:38 +0900

    Uplaod passport.js

--- passport.js
+++ passport.js
@@ -7,12 +7,14 @@ import {
 import routes from "./routes";
 passport.use(User.createStrategy());
 
-passport.use(new GithubStrategy({
-        clientID: process.env.GH_ID,
-        clientSecret: process.env.GH_SECRET,
-        callbackURL: `http://localhost:4000${routes.githubLoginCallback}`
-    }),
-    githubLoginCallback
+passport.use(
+    new GithubStrategy({
+            clientID: process.env.GH_ID,
+            clientSecret: process.env.GH_SECRET,
+            callbackURL: `http://localhost:4000${routes.githubLoginCallback}`
+        },
+        githubLoginCallback
+    )
 );
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
