619cf02789401f5e1cbcd34dfc703a59250a3efc
김민성 <yssj2049@gmail.com>
2019-02-07 00:09:40 +0900

    Update callbackURL

--- passport.js
+++ passport.js
@@ -4,12 +4,13 @@ import GithubStrategy from "passport-github";
 import {
     githubLoginCallback
 } from "./controllers/userController";
+import routes from "./routes";
 passport.use(User.createStrategy());
 
 passport.use(new GithubStrategy({
         clientID: process.env.GH_ID,
         clientSecret: process.env.GH_SECRET,
-        callbackURL: "http://localhost:4000/auth/github/callback"
+        callbackURL: `http://localhost:4000${routes.githubLoginCallback}`
     }),
     githubLoginCallback
 );
